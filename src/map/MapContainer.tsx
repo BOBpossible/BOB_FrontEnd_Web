f076ab97a6de6267178fbfd8d2f2f126a71b3a46
yejinleee <yejin9487@gmail.com>
2022-06-18 00:26:05 +0900

    Feat: marker + infro window

--- src/map/MapContainer.tsx
+++ src/map/MapContainer.tsx
@@ -6,6 +6,9 @@ declare global {
     kakao: any;
   }
 }
+interface mapData {
+  name: string,
+}
 
 const MapContainer = () => {
     useEffect(() => {
@@ -19,15 +22,37 @@ const MapContainer = () => {
           displayMarker(dummy.data[i],i);
         }
         
-        function displayMarker(data: object, i: number) {
+        function displayMarker<T extends {name: string, location_y: number, location_x: number, active: boolean}>(data: T, i: number) {
           // 마커가 표시될 위치입니다 
-          let markerPosition  = new window.kakao.maps.LatLng(dummy.data[i].location_y, dummy.data[i].location_x); 
+          let markerPosition  = new window.kakao.maps.LatLng(data.location_y, data.location_x); 
         
-          // 마커를 생성합니다
+          // 마커를 생성합니다 (핀 모양!)
           let marker = new window.kakao.maps.Marker({
               position: markerPosition,
               map: map, // 마커가 지도 위에 표시되도록 설정합니다
           });
+
+           // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+          var activeInfoWindow = '<div style="padding:5px;">Hello World!</div>';
+          var unactiveInfoWindow = '<div style="padding:5px;">nooo!</div>';
+
+          //인포윈도우
+          var infowindow = new window.kakao.maps.InfoWindow({
+            zIndex: 1,
+            position: markerPosition,
+            content: activeInfoWindow
+          });
+          infowindow.open(map) //(map,marker)하면 마커(핀)도 나타납니다.
+
+          //클릭이벤트
+          // window.kakao.maps.event.addListener(marker, 'click', function () {
+          //   if (data.active) {
+          //     infowindow.setContent('<div style="padding:5px;font-size:12px;">' + data.name + '</div>')
+          //   } else {
+          //     infowindow.setContent('<div style="padding:2px;font-size:5px;">' + data.name + '</div>')
+          //   }
+          //   infowindow.open(map, marker)
+          // })
         }
       }, [])
 
