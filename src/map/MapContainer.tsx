5ee92f3d6b9072a73c4b92ab98c5c3d8f45599e6
yejinleee <yejin9487@gmail.com>
2022-06-20 15:55:55 +0900

    Feat: geocoder.addressSearch

--- src/map/MapContainer.tsx
+++ src/map/MapContainer.tsx
@@ -8,107 +8,88 @@ declare global {
     kakao: any;
   }
 }
-interface mapData {
-  name: string,
-}
 
 const MapContainer = () => {
     useEffect(() => {
         let container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
         let options = { //지도를 생성할 때 필요한 기본 옵션
           center: new window.kakao.maps.LatLng(37.586272, 127.029005), //지도의 중심좌표. ((안암역))
-          level: 1 //지도의 레벨(확대, 축소 정도)
+          level: 3 //지도의 레벨(확대, 축소 정도)
         };
         let map = new window.kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+        let geocoder = new window.kakao.maps.services.Geocoder();
 
         for (let i=0; i< dummy.data.length; i++){
-          displayMarker(dummy.data[i],i);
+
+          // 주소로 좌표를 검색합니다
+          geocoder.addressSearch(dummy.data[i].address, function(result:any, status:any) {
+            // 정상적으로 검색이 완료됐으면 
+            if (status === window.kakao.maps.services.Status.OK) {
+              var coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+
+              // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+              var activeInfoWindow = `<div class="active infowindow""><span class="point">${dummy.data[i].point}P</span><span>${dummy.data[i].name}</span></div>`;
+              var inactiveInfoWindow = `<div class="inactive infowindow"><span>${dummy.data[i].name}</span></div>`;
+
+              //인포윈도우
+              let infowindow;
+              if (dummy.data[i].active) {
+                infowindow = new window.kakao.maps.InfoWindow({
+                  zIndex: 1,
+                  position: coords,
+                  content: activeInfoWindow,
+                  disableAutoPan: false,
+                  map: map
+                });
+              } else{
+                infowindow = new window.kakao.maps.InfoWindow({
+                  zIndex: 1,
+                  position: coords,
+                  content: inactiveInfoWindow,
+                  disableAutoPan: false,
+                  map: map
+                });
+              }
+              var position = new window.kakao.maps.LatLng(37.586272, 127.029005);
+              map.setCenter(position); //중심좌표 재설정
+              
+              var infoTitle = document.querySelectorAll('.infowindow');
+              infoTitle.forEach(function(e: any) {
+                // console.log(e,e.className.includes('inactive'));
+                var w = e.offsetWidth + 10;
+                e.parentElement.style.width = w;  
+                e.parentElement.style.position = "relative";
+                if (e.className.includes('inactive')){//비활성화 핀
+                  e.parentElement.style.top = "3px";
+                  e.parentElement.previousSibling.style.backgroundImage = "url('https://user-images.githubusercontent.com/81412212/174342201-0ec0c927-97f1-49dd-8c23-d6a872d9dfad.png')"; //꼭지
+                } else {
+                  e.parentElement.previousSibling.style.backgroundImage = "url('https://user-images.githubusercontent.com/81412212/174341207-bbaa6a46-2d67-4731-8a51-9a429488affa.png')"; //꼭지
+                  e.childNodes[1].style.display = "block";
+                  e.childNodes[1].style.margin ="-8px";
+                  e.parentElement.style.top = "-12px";
+                }
+                e.parentElement.parentElement.style.display = "flex"; //부모(기본인포윈도우영역)
+                e.parentElement.parentElement.style.background = "none"; //부모(기본인포윈도우영역)
+                e.parentElement.parentElement.style.border = "none"; //부모(기본인포윈도우영역)
+                e.parentElement.parentElement.style.justifyContent = "center"; //부모(기본인포윈도우영역)
+              });
+              
+            } else{
+              console.log('주소 잘못됨: ', dummy.data[i].address)
+            }
+          }); 
+
         }
         
-        function displayMarker<T extends {name: string, location_y: number, location_x: number, active: boolean, point:number}>(data: T, i: number) {
-          // console.log(data);
-          // console.log(data.active);
-          // 인포윈도우 표시될 위치입니다 
-          let iwPosition  = new window.kakao.maps.LatLng(data.location_y, data.location_x); 
+        // function displayMarker<T extends {name: string, address:string, location_y: number, location_x: number, active: boolean, point:number}>(data: T, i: number) {
+        //   var position = new window.kakao.maps.LatLng(37.586272, 127.029005);
+        //   map.setCenter(position); //중심좌표 재설정
 
-           // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-          var activeInfoWindow = `<div class="active infowindow""><span class="point">${data.point}P</span><span>${data.name}</span></div>`;
-          var inactiveInfoWindow = `<div class="inactive infowindow"><span>${data.name}</span></div>`;
-          // const infowindowStyle = {{
+        //   //클릭이벤트
+        //   // window.kakao.maps.event.addListener(marker, 'click', function () {
+        //   // })
+        // }
 
-          // }}
-
-          //인포윈도우
-          let infowindow;
-          if (data.active) {
-            infowindow = new window.kakao.maps.InfoWindow({
-              zIndex: 1,
-              position: iwPosition,
-              content: activeInfoWindow,
-              disableAutoPan: false,
-              map: map
-            });
-          } else{
-            infowindow = new window.kakao.maps.InfoWindow({
-              zIndex: 1,
-              position: iwPosition,
-              content: inactiveInfoWindow,
-              disableAutoPan: false,
-              map: map
-            });
-          }
-
-          var position = new window.kakao.maps.LatLng(37.586272, 127.029005);
-          map.setCenter(position); //중심좌표 재설정
-
-          //클릭이벤트
-          // window.kakao.maps.event.addListener(marker, 'click', function () {
-          //   if (data.active) {
-          //     infowindow.setContent('<div style="padding:5px;font-size:12px;">' + data.name + '</div>')
-          //   } else {
-          //     infowindow.setContent('<div style="padding:2px;font-size:5px;">' + data.name + '</div>')
-          //   }
-          //   infowindow.open(map, marker)
-          // })
-        }
-
-        // https://sir.kr/g5_tip/14200
-        var infoTitle = document.querySelectorAll('.infowindow');
-        infoTitle.forEach(function(e: any) {
-          // console.log(e,e.className.includes('inactive'));
-          var w = e.offsetWidth + 10;
-          var ml = w/2;
-          e.parentElement.style.width = w;  
-          // e.parentElement.style.top = "82px";
-          e.parentElement.style.position = "relative";
-          // e.parentElement.style.left = "50%";
-          // e.parentElement.style.marginLeft = -ml+"px";
-          // e.parentElement.style.width = w+"px";
-          console.log(e.parentElement.previousSibling);
-          if (e.className.includes('inactive')){
-            e.parentElement.previousSibling.style.backgroundImage = "url('https://user-images.githubusercontent.com/81412212/174342201-0ec0c927-97f1-49dd-8c23-d6a872d9dfad.png')"; //꼭지
-          } else {
-            e.parentElement.previousSibling.style.backgroundImage = "url('https://user-images.githubusercontent.com/81412212/174341207-bbaa6a46-2d67-4731-8a51-9a429488affa.png')"; //꼭지
-          }
-          e.parentElement.parentElement.style.display = "flex"; //부모(기본인포윈도우영역)
-          e.parentElement.parentElement.style.background = "none"; //부모(기본인포윈도우영역)
-          e.parentElement.parentElement.style.border = "none"; //부모(기본인포윈도우영역)
-          e.parentElement.parentElement.style.justifyContent = "center"; //부모(기본인포윈도우영역)
-          // e.parentElement.parentElement.style.border = "0px";
-          // e.parentElement.parentElement.style.background = "unset";
-          // console.log(e.childNodes.length);
-          if (e.childNodes.length===2){ //활성화된 핀
-            e.childNodes[1].style.display = "block";
-            e.childNodes[1].style.margin ="-8px";
-            e.parentElement.style.top = "-12px";
-            // e.parentElement.previousSibling.style.top = "22px"; //꼭지
-          } else{
-            e.parentElement.style.top = "3px";
-            // e.parentElement.previousSibling.style.top = "20px"; //꼭지
-          }
-
-
-        });
       }, [])
 
     return (
