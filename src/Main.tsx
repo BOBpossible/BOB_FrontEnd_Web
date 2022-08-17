f51d947c3d2ad85e1b4c3e6877a2a0eb9afb4e13
yejinleee <yejin9487@gmail.com>
2022-08-17 17:57:48 +0900

    Design: padding

--- src/Main.tsx
+++ src/Main.tsx
@@ -9,18 +9,18 @@ const Main = () => {
   
   return (
     <>
-        <div style={{width: '100vw', height: '100vh', background:'#6C69FF', textAlign: 'center'}}>
+        <div style={{width: '100vw', height: '100vh', background:'#6C69FF', textAlign: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
 
-            <div style={{margin: '0 auto'}}>
-              <img style={{width: '402px', display: 'inline-block'}} src={bobplace} alt="bobplace" />
-              <img style={{width: '402px'}} src={bobtext} alt="bobtext" />
+            <div style={{margin: '0 auto', paddingTop: '15vh'}}>
+              <img style={{width: '402px', display: 'block'}} src={bobplace} alt="bobplace" />
+              <img style={{width: '402px', paddingTop: '1vh'}} src={bobtext} alt="bobtext" />
             </div>
             <div style={{margin: '0 auto'}}>
                 <img style={{width: '200px', height:'60px', marginRight: 14}} src={apple} alt="AppStore" />
                 <img style={{width: '200px', height:'60px'}} src={googleplay} alt="GooglePlay" />
             </div>
             <div style={{margin: '0 auto'}}>
-              <img style={{width: '100vw'}} src={bobpool} alt="bobpool" />
+              <img style={{width: '75vw'}} src={bobpool} alt="bobpool" />
             </div>
         </div>
     </>
